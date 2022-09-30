42a50a75b3160cfe37e9900d382a5442f6b69c9c
Julian Naranjo R <naranjojulian702@gmail.com>
2022-09-30 16:09:54 -0700

    removed the galleries button and page because all of the material provided was already used in the landing page. also, changed some backgrounds to make the pages more consistent.

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-const leftBtn = document.querySelector('.leftBtn'); 
+const leftBtn = document.querySelector(".leftBtn");
 
-const middleBtn = document.querySelector('.middleBtn'); 
+const middleBtn = document.querySelector(".middleBtn");
 
-const rightBtn = document.querySelector('.rightBtn'); 
+const rightBtn = document.querySelector(".rightBtn");
 
-const galleryGrid1 = document.querySelector('.galleryGrid1'); 
+const galleryGrid1 = document.querySelector(".galleryGrid1");
 
-const galleryGrid2 = document.querySelector('.galleryGrid2'); 
+const galleryGrid2 = document.querySelector(".galleryGrid2");
 
-const galleryGrid3 = document.querySelector('.galleryGrid3'); 
+const galleryGrid3 = document.querySelector(".galleryGrid3");
 
-//these EL's help the gallery on landing page work: 
+//these EL's help the gallery on landing page work:
 
-leftBtn.addEventListener('click', () => {
-    // local 
-    galleryGrid1.style.display = 'grid'; 
-    //other grids
-    galleryGrid2.style.display = 'none';
-    galleryGrid3.style.display = 'none'; 
-})
-middleBtn.addEventListener('click', () => {
-    //local grid 
-    galleryGrid2.style.display = 'grid'; 
-    //other grids: 
-    galleryGrid1.style.display = 'none'; 
-    galleryGrid3.style.display = 'none'; 
-})
-rightBtn.addEventListener('click', () => {
-    //local grid
-    galleryGrid3.style.display = 'grid'; 
-    //other grids: 
-    galleryGrid1.style.display = 'none'; 
-    galleryGrid2.style.display = 'none'; 
-})
-
+leftBtn.addEventListener("click", () => {
+  // local
+  galleryGrid1.style.display = "grid";
+  //other grids
+  galleryGrid2.style.display = "none";
+  galleryGrid3.style.display = "none";
+});
+middleBtn.addEventListener("click", () => {
+  //local grid
+  galleryGrid2.style.display = "grid";
+  //other grids:
+  galleryGrid1.style.display = "none";
+  galleryGrid3.style.display = "none";
+});
+rightBtn.addEventListener("click", () => {
+  //local grid
+  galleryGrid3.style.display = "grid";
+  //other grids:
+  galleryGrid1.style.display = "none";
+  galleryGrid2.style.display = "none";
+});
